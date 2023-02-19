ed67464271b117063dc0d2b08ebcbfd5f26c6e55
nguyenhuykhang <54362200+nguyenhuykhang@users.noreply.github.com>
2023-02-19 10:55:23 -0500

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore'
 const firebaseConfig = {
-    apiKey: "AIzaSyDIFVpC3qZVOzgiMatKYAVztPD3RHlmSBE",
-    authDomain: "mhacks-22493.firebaseapp.com",
-    projectId: "mhacks-22493",
-    storageBucket: "mhacks-22493.appspot.com",
-    messagingSenderId: "208233282818",
-    appId: "1:208233282818:web:be074369301831f144961b",
-    measurementId: "G-H7C211N2CY"
+    //add yours
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
