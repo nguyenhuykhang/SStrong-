958eb45d7f33a7b2a4cf9dbc250aaf71d474c07b
nguyenhuykhang <54362200+nguyenhuykhang@users.noreply.github.com>
2023-02-20 15:44:33 -0500

    Update twilio.py

--- src/twilio.py
+++ src/twilio.py
@@ -1,15 +1,14 @@
 from twilio.rest import Client
 
-account_sid = 'ACf164d7e16fdee6743427f514dca24a6e'
-auth_token = 'a546b6a31e2aaf8a0903535cb68cb6ad'
+account_sid = 'ADD_YOURS'
+auth_token = 'ADD_YOURS'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
-                     body="Your friend Nguyen Huy Khang is in danger. Location is 234 Wilson Rd, East Lansing, "
-                          "MI 48825, USA",
-                     from_='+18559734060',
-                     to='+15174026152   '
+                     body="Your friend ADD_YOURS is in danger. Location is ADD_YOURS",
+                     from_='ADD_YOURS',
+                     to='ADD_YOURS'   '
                  )
 
 print(message.sid)
